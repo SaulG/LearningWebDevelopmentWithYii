17c56ac1968402e2d884fb014dc553cd862d408b
saul <saul.gausin@gmail.com>
2014-08-18 16:45:45 -0500

    Add user access control

--- trackapplication/protected/models/LoginForm.php
+++ trackapplication/protected/models/LoginForm.php
@@ -67,9 +67,9 @@ class LoginForm extends CFormModel
 		}
 		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
 		{
-			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
-			Yii::app()->user->login($this->_identity,$duration);
-			return true;
+                    $duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
+                    Yii::app()->user->login($this->_identity,$duration);
+                    return true;
 		}
 		else
 			return false;
